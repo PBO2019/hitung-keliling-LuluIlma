4677f539d49304d6dfe44c2f870e398a28999649
LuluIlma <ilulu2247@gmail.com>
2019-01-24 09:44:44 +0700

    Latihan Menghitung Keliling Bujur Sangkar

---
+++ src/com/company/BujurSangkar.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class BujurSangkar {
+    public static void main(String[]args){
+        int sisi =10;
+        System.out.print(" Keliling Bujur Sangkar" + 4*sisi);
+    }
+}
